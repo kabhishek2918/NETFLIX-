5d0463f54b401a6cd601a48e160f4960eee15dd6
abhishek <kabhishek2918@outlook.com>
2025-03-20 01:50:51 +0530

    Updated LoginPage UI and Functionality

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/authUser";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { login, isLoggingIn } = useAuthStore();
+
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login({ email, password });
   };
+
   return (
     <div className="h-screen w-full hero-bg">
       <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
         <Link to={"/"}>
-          <img src="netflix-logo.png" alt="logo" className="w-52" />
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
         </Link>
       </header>
 
@@ -51,15 +55,20 @@ const LoginPage = () => {
               <input
                 type="password"
                 className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
-                placeholder="***********"
+                placeholder="••••••••"
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
-            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
-              Login
+            <button
+              className="w-full py-2 bg-red-600 text-white font-semibold rounded-md
+							hover:bg-red-700
+						"
+              disabled={isLoggingIn}
+            >
+              {isLoggingIn ? "Loading..." : "Login"}
             </button>
           </form>
           <div className="text-center text-gray-400">
@@ -73,5 +82,4 @@ const LoginPage = () => {
     </div>
   );
 };
-
 export default LoginPage;
