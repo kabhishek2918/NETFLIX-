b7635cdb9882141bcd81991bd766fcdcde2b60b8
abhishek <kabhishek2918@outlook.com>
2025-03-20 01:21:30 +0530

    Updated feature X or fixed bug Y

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -5,6 +5,9 @@ import { create } from "zustand";
 export const useAuthStore = create((set) => ({
   user: null,
   isSigningUp: false,
+  isCheckingAuth: true,
+  isLoggingOut: false,
+  isLoggingIn: false,
   signup: async (credentials) => {
     set({ isSigningUp: true });
     try {
@@ -12,11 +15,40 @@ export const useAuthStore = create((set) => ({
       set({ user: response.data.user, isSigningUp: false });
       toast.success("Account created successfully");
     } catch (error) {
-      toast.error(error.response.data.message || " An error occurred");
       set({ isSigningUp: false, user: null });
+      toast.error(error.response.data.message || " Signup Failed");
     }
   },
-  login: async () => {},
-  logout: async () => {},
-  authCheck: async () => {},
+  login: async (credentials) => {
+    set({ isLoggingIn: true });
+    try {
+      const response = await axios.post("/api/v1/auth/login", credentials);
+      set({ user: response.data.user, isLoggingIn: false });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      set({ isLoggingIn: false, user: null });
+      toast.error(error.response.data.message || " Login Failed");
+    }
+  },
+  logout: async () => {
+    set({ isLoggingOut: true });
+    try {
+      await axios.post("/api/v1/auth/logout");
+      set({ user: null, isLoggingOut: false });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      set({ isLoggingOut: false });
+      toast.error(error.response.data.message || " Logout Failed");
+    }
+  },
+  authCheck: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const response = await axios.get("/api/v1/auth/authCheck");
+      set({ user: response.data.user, isCheckingAuth: false });
+    } catch (error) {
+      set({ user: null, isCheckingAuth: false });
+      // toast.error(error.response.data.message || " An error occurred");
+    }
+  },
 }));
